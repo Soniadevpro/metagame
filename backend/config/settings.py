72b2fa8216afdb6a7cdd6c715d6d706c76085c15
Sonia <soniadevpro@gmail.com>
2025-03-04 23:52:33 +0100

    configuration docker

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -58,8 +58,8 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB', 'metagame'),
         'USER': os.getenv('POSTGRES_USER', 'metagame_user'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'metagame_password'),
-        'HOST': 'db',
-        'PORT': '5432',
+        'HOST': os.getenv('POSTGRES_HOST', 'db'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
@@ -82,3 +82,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Configuration CORS
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Metagame</h1>
+      <p>Projet en cours de développement</p>
+    </div>
+  )
+}
+
+export default App
